09751b41c0bc5ad78d73e6e3c2e162150c86e1e0
niteen <ndhule1988@gmail.com>
2023-11-05 23:28:37 +0530

    added new enum for series and updated the cricket format enum

--- src/main/java/com/icc/cricket/common/CricketFormat.java
+++ src/main/java/com/icc/cricket/common/CricketFormat.java
@@ -1,5 +1,49 @@
 package com.icc.cricket.common;
 
+import java.security.InvalidParameterException;
+
 public enum CricketFormat {
-    ONE_DAY, TEST, T20
+    ONE_DAY("1", "1D", "One Day"),
+    TEST("2", "TEST", "Test"),
+    T20("3", "T20", "Twenty20");
+
+    private String id;
+    private String displayName;
+    private String code;
+    CricketFormat(String id, String code, String displayName) {
+        this.id = id;
+        this.code = code;
+        this.displayName = displayName;
+    }
+    public CricketFormat getCricketFormatById(String id) {
+        for (CricketFormat cf: CricketFormat.values()) {
+            if(cf.id.equals(id)) {
+                return cf;
+            }
+        }
+        throw new InvalidParameterException("No cricket format exist for given id "+id);
+    }
+    public String getCodeById(String id) {
+        for (CricketFormat cf: CricketFormat.values()) {
+            if(cf.id.equals(id)) {
+                return cf.getCode();
+            }
+        }
+        throw new InvalidParameterException("No cricket format exist for given id "+id);
+    }
+    public String getDisplayNameById(String id) {
+        for (CricketFormat cf: CricketFormat.values()) {
+            if(cf.id.equals(id)) {
+                return cf.getDisplayName();
+            }
+        }
+        throw new InvalidParameterException("No cricket format exist for given id "+id);
+    }
+
+    private String getDisplayName(){
+        return this.displayName;
+    }
+    private String getCode(){
+        return this.code;
+    }
 }
